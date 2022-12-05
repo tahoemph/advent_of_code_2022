21190ed6342bc9bf22cce77b99898c556a8f82d8
Michael Hunter <tahoemph@gmail.com>
2022-12-04 22:59:56 -0800

    day 1

---
+++ 01/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] lines = File.ReadAllLines("data.txt");
+List<int> counts = new List<int>();
+
+int total = 0;
+foreach (string line in lines) {
+  if (line == "") {
+    counts.Add(total);
+    total = 0;
+  } else {
+    total += Int32.Parse(line);
+  }
+}
+
+int FirstMax = counts.Max(x => x);
+counts.Remove(FirstMax);
+
+int SecondMax = counts.Max(x => x);
+counts.Remove(SecondMax);
+
+int ThirdMax = counts.Max(x => x);
+
+Console.WriteLine(FirstMax);
+Console.WriteLine(SecondMax);
+Console.WriteLine(ThirdMax);
+Console.WriteLine(FirstMax + SecondMax + ThirdMax);
